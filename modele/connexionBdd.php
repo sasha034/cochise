60abf1ef0c74266ecd99b0ac97864dcec6d227ea
Maxime <13Mampalen>
2016-05-27 13:34:11 +0200

    base distante

--- modele/connexionBdd.php
+++ modele/connexionBdd.php
@@ -1,10 +1,27 @@
 <?php
-//Fichier de connexion à la base de données MySql
-try {
-    $bdd = new PDO('mysql:host=localhost;dbname=cochise;charset=utf8', 'root', '');
-    } 
-catch (Exception $ex) {
-    die('Erreur : ' . $e->getMessage());
+//Fichier de connexion ï¿½ la base de donnï¿½es MySql
+$local = false;
+
+if($local){
+    try {
+        $bdd = new PDO('mysql:host=localhost;dbname=cochise;charset=utf8', 'root', '');
+    }
+    catch (Exception $ex) {
+        die('Erreur : ' . $ex->getMessage());
+    }
+} else {
+    try {
+        $servername = "mysql-daskopls.alwaysdata.net";
+        $username = "daskopls";
+        $password = "max18ime";
+        $port = "";
+        $db = "daskopls_cochise";
+
+        $bdd =  new PDO("mysql:host=$servername$port;dbname=$db;charset=utf8", $username, $password);
+    } catch (Exception $ex) {
+        die('Erreur : ' . $ex->getMessage());
+    }
 }
+
 
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
