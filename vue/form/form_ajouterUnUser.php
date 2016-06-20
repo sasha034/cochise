92b057254767ba296fa4f69a65c79fb3c4c0cf9f
Maxime <13Mampalen>
2016-06-20 13:29:09 +0200

    css

--- vue/form/form_ajouterUnUser.php
+++ vue/form/form_ajouterUnUser.php
@@ -19,14 +19,32 @@ include_once '../../modele/connexionBdd.php';
             <form id="form_ajouterUnUser" name="form_ajouterUnUser" action="../../controller/user.action.ajouter.php" method="POST" onsubmit=" return checkRegex();">
                 <div id="intro">
                     <p class="h2style">Bienvenue sur votre formulaire d'inscription</p>
-                </div>               
-                    <label id="login" for="login">Login * :</label><input type="text" name="login" id="login" /><br />
-                    <label id="password" for="password">Mot de passe * :</label><input type="password" name="password" id="password" /><br />
-                    <label id="nom_user" for="nom_user">Nom * :</label><input type="text" name="nom_user" id="nom_user" /><br />
-                    <label id="email_user" for="email_user">Email *:</label><input type="text" name="email_user" id="email_user" /><br />
-                    <p><input class='bouton_validation' type="submit" value="Valider inscription" name="bouton_validation"/></p>
+                </div>
+                <table class="lol">
+                    <tr>
+                        <td><label id="login" for="login">Login * :</label></td>
+                        <td><input type="text" name="login" id="login" /></td>
+                    </tr>
+                    <tr>
+                        <td><label id="password" for="password">Mot de passe * :</label></td>
+                        <td><input type="password" name="password" id="password" /></td>
+                    </tr>
+                    <tr>
+                        <td><label id="nom_user" for="nom_user">Nom * :</label></td>
+                        <td><input type="text" name="nom_user" id="nom_user" /></td>
+                    </tr>
+                    <tr>
+                        <td><label id="email_user" for="email_user">Email *:</label></td>
+                        <td><input type="text" name="email_user" id="email_user" /></td>
+                    </tr>
+                    <tr style="height: 40px">
+                        <td colspan="2"><input class='bouton_validation' type="submit" value="Valider inscription" name="bouton_validation"/></td>
+                    </tr>
+                </table>
+
+                    <a href="../../index.php">Retour</a>
                     <p>(*) champs obligatoires</p>
-                    <div><a href="../../index.php">Retour</a></div>               
+                    
             </form>
         </div>
     </body>
